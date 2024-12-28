10170bdc9920e8bd27ff6a0d816d5489868309f2
velkani <velmuthu528494@gmail.com>
2024-12-28 12:26:59 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
